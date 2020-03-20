95b5f8321d7158e533015f9cb0be5656993d40c9
Piotr Majkrzak <piotr@majkrzak.dev>
2020-03-20 19:30:17 +0200

    Implement run application

--- src/Majkrzak/Maestro.hs
+++ src/Majkrzak/Maestro.hs
@@ -5,7 +5,9 @@ module Majkrzak.Maestro
   , Action
   , dump
   , TopicMapping(TopicMapping)
+  , (/+/)
   , (/-/)
+  , runApplication
   )
 where
 
@@ -13,13 +15,55 @@ import Majkrzak.Maestro.Utils
 import Majkrzak.Maestro.Types
 
 import Data.Text (Text, pack)
+import Network.MQTT.Client
+  ( connectURI
+  , _msgCB
+  , mqttConfig
+  , MessageCallback(SimpleCallback)
+  , publish
+  , waitForClient
+  , subscribe
+  , subOptions
+  )
+import Network.URI (URI)
+import Control.Concurrent.STM (atomically, newTVar, readTVar, writeTVar)
+import Control.Monad (void)
 
 
-data TopicMapping = TopicMapping Text Text
+data TopicMapping = TopicMapping Bool Text Text
+
+(/+/) :: RName a => Text -> a -> TopicMapping
+l /+/ r = TopicMapping True l $ pack $ name r
 
 (/-/) :: RName a => Text -> a -> TopicMapping
-l /-/ r = TopicMapping l $ pack $ name r
+l /-/ r = TopicMapping False l $ pack $ name r
 
-data Application = Application
-  {
+data Application s e a = Application
+  { init :: s
+  , next :: (Event e, Action a) => s -> e -> (s, [a])
+  , maps :: [TopicMapping]
+  , mqtt :: URI
   }
+
+
+runApplication :: (Event e, Action a) => Application s e a -> IO ()
+runApplication Application {..} = do
+  state <- atomically . newTVar $ init
+  mc    <- connectURI
+    mqttConfig
+      { _msgCB = SimpleCallback $ \mc topic payload _ -> do
+        event   <- load topic payload
+        actions <- atomically $ do
+          oldState <- readTVar state
+          let (newState, actions) = next oldState event
+          writeTVar state newState
+          return actions
+        mapM_
+          ((\(topic', payload') -> publish mc topic' payload' False) . dump)
+          actions
+      }
+    mqtt
+  let filtered = filter (\(TopicMapping s _ _) -> s) maps
+  let mapped   = map (\(TopicMapping _ t _) -> (t, subOptions)) filtered
+  void $ subscribe mc mapped []
+  waitForClient mc
